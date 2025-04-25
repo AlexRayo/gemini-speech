50c98fb5145869f30a147d5019adf929d0b6c2b7
asdf <alexrayo.net@gmail.com>
2025-04-25 16:37:13 -0600

    metro and babel config to build apk

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,31 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      "nativewind/babel"
+    ],
+    plugins: [
+      ['module-resolver', {
+        root: ['./'],              // la raíz de tu proyecto
+        extensions: [
+          '.ios.js', '.android.js', '.js', '.ts', '.tsx',
+          '.json', '.jsx', '.native.js'
+        ],
+        alias: {
+          '@/components': './components',
+          '@/constants':     './constants',
+          '@/app':        './app',
+          '@/hooks':      './hooks',
+          '@/assets':     './assets'
+        },
+      }],
+      // otros plugins que necesites…
     ],
     env: {
       production: {
-        plugins: ['react-native-paper/babel'],
-      },
+        plugins: ['react-native-paper/babel']
+      }
     },
   };
 };
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
-const config = getDefaultConfig(__dirname)
-
-module.exports = withNativeWind(config, { input: './global.css' })
+const config = getDefaultConfig(__dirname);
+config.resolver.alias = {
+  '@/components': path.resolve(__dirname, 'components'),
+  '@/constants': path.resolve(__dirname, 'constants'),
+  '@/app':        path.resolve(__dirname, 'app'),
+  '@/hooks':      path.resolve(__dirname, 'hooks'),
+  '@/assets':     path.resolve(__dirname, 'assets'),
+};
+module.exports = withNativeWind(config, { input: './global.css' });
